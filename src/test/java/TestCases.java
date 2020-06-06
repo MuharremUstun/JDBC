f83bdff827c3148a8f009ee6141d5590b423c712
MuharremUstun <56238963+MuharremUstun@users.noreply.github.com>
2020-06-06 00:53:09 -0400

    connected to database received students from db

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,3 +1,26 @@
-public class TestCases {
+import org.testng.annotations.Test;
 
+import java.sql.*;
+
+public class TestCases {
+    @Test
+    public void test() throws SQLException {
+        String host = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/";
+        String database = "muharremustun_students_db";
+        String url = host.concat(database);
+//      String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/muharremustun_students_db";
+        String user = "muharremustun";
+        String password = "muharremustun@gmail.com";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        ResultSet rs= statement.executeQuery("SELECT first_name, last_name, email FROM students;");
+        while (rs.next()){
+            String name =rs.getString(1);
+            String lastName =rs.getString(2);
+            String email =rs.getString(3);
+            System.out.println(name + " " + lastName + " " + email);
+        }
+
+
+    }
 }
